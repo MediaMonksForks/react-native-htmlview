223d6ab7a10c9a690907f066fba804e95a2b3095
Tom Atterton <toma@mediamonks.com>
2018-04-26 11:12:13 +0200

    Hack to get inline styles "color" working

--- htmlToElement.js
+++ htmlToElement.js
@@ -1,37 +1,51 @@
-import React from 'react';
-import {StyleSheet, Text} from 'react-native';
-import htmlparser from 'htmlparser2-without-node-native';
-import entities from 'entities';
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+import htmlparser from "htmlparser2-without-node-native";
+import entities from "entities";
 
-import AutoSizedImage from './AutoSizedImage';
+import AutoSizedImage from "./AutoSizedImage";
 
 const defaultOpts = {
-  lineBreak: '\n',
-  paragraphBreak: '\n\n',
-  bullet: '\u2022 ',
+  lineBreak: "\n",
+  paragraphBreak: "\n\n",
+  bullet: "\u2022 ",
   TextComponent: Text,
   textComponentProps: null,
   NodeComponent: Text,
-  nodeComponentProps: null,
+  nodeComponentProps: null
+};
+
+const nodeStyles = styleProp => {
+  if (styleProp) {
+    const customStyle = styleProp.split(": ");
+    if (customStyle[0] === "color") {
+      const value = customStyle[1].replace(";", "");
+      return {
+        [customStyle[0]]: value
+      };
+    }
+    return {};
+  }
+  return {};
 };
 
 const Img = props => {
   const width =
-    parseInt(props.attribs['width'], 10) || parseInt(props.attribs['data-width'], 10) || 0;
+    parseInt(props.attribs["width"], 10) || parseInt(props.attribs["data-width"], 10) || 0;
   const height =
-    parseInt(props.attribs['height'], 10) ||
-    parseInt(props.attribs['data-height'], 10) ||
+    parseInt(props.attribs["height"], 10) ||
+    parseInt(props.attribs["data-height"], 10) ||
     0;
 
   const imgStyle = {
     width,
-    height,
+    height
   };
 
   const source = {
     uri: props.attribs.src,
     width,
-    height,
+    height
   };
   return <AutoSizedImage source={source} style={imgStyle} />;
 };
@@ -39,18 +53,22 @@ const Img = props => {
 export default function htmlToElement(rawHtml, customOpts = {}, done) {
   const opts = {
     ...defaultOpts,
-    ...customOpts,
+    ...customOpts
   };
 
   function inheritedStyle(parent) {
-    if (!parent) return null;
+    if (!parent) {
+      return null;
+    }
     const style = StyleSheet.flatten(opts.styles[parent.name]) || {};
     const parentStyle = inheritedStyle(parent.parent) || {};
-    return {...parentStyle, ...style};
+    return { ...parentStyle, ...style };
   }
 
   function domToElement(dom, parent) {
-    if (!dom) return null;
+    if (!dom) {
+      return null;
+    }
 
     const renderNode = opts.customRenderer;
     let orderedListCounter = 1;
@@ -64,14 +82,17 @@ export default function htmlToElement(rawHtml, customOpts = {}, done) {
           parent,
           domToElement
         );
-        if (rendered || rendered === null) return rendered;
+        if (rendered || rendered === null) {
+          return rendered;
+        }
       }
 
-      const {TextComponent} = opts;
+      const { TextComponent } = opts;
 
-      if (node.type === 'text') {
+      if (node.type === "text") {
         const defaultStyle = opts.textComponentProps ? opts.textComponentProps.style : null;
         const customStyle = inheritedStyle(parent);
+        const nodeStyle = nodeStyles(node.parent && node.parent.attribs['style']);
 
         return (
           <TextComponent
@@ -84,14 +105,14 @@ export default function htmlToElement(rawHtml, customOpts = {}, done) {
         );
       }
 
-      if (node.type === 'tag') {
-        if (node.name === 'img') {
+      if (node.type === "tag") {
+        if (node.name === "img") {
           return <Img key={index} attribs={node.attribs} />;
         }
 
         let linkPressHandler = null;
         let linkLongPressHandler = null;
-        if (node.name === 'a' && node.attribs && node.attribs.href) {
+        if (node.name === "a" && node.attribs && node.attribs.href) {
           linkPressHandler = () =>
             opts.linkHandler(entities.decodeHTML(node.attribs.href));
           if (opts.linkLongPressHandler) {
@@ -104,35 +125,35 @@ export default function htmlToElement(rawHtml, customOpts = {}, done) {
         let linebreakAfter = null;
         if (opts.addLineBreaks) {
           switch (node.name) {
-          case 'pre':
-            linebreakBefore = opts.lineBreak;
-            break;
-          case 'p':
-            if (index < list.length - 1) {
-              linebreakAfter = opts.paragraphBreak;
-            }
-            break;
-          case 'br':
-          case 'h1':
-          case 'h2':
-          case 'h3':
-          case 'h4':
-          case 'h5':
-            linebreakAfter = opts.lineBreak;
-            break;
+            case "pre":
+              linebreakBefore = opts.lineBreak;
+              break;
+            case "p":
+              if (index < list.length - 1) {
+                linebreakAfter = opts.paragraphBreak;
+              }
+              break;
+            case "br":
+            case "h1":
+            case "h2":
+            case "h3":
+            case "h4":
+            case "h5":
+              linebreakAfter = opts.lineBreak;
+              break;
           }
         }
 
         let listItemPrefix = null;
-        if (node.name === 'li') {
+        if (node.name === "li") {
           const defaultStyle = opts.textComponentProps ? opts.textComponentProps.style : null;
           const customStyle = inheritedStyle(parent);
 
-          if (parent.name === 'ol') {
+          if (parent.name === "ol") {
             listItemPrefix = (<TextComponent style={[defaultStyle, customStyle]}>
               {`${orderedListCounter++}. `}
             </TextComponent>);
-          } else if (parent.name === 'ul') {
+          } else if (parent.name === "ul") {
             listItemPrefix = (<TextComponent style={[defaultStyle, customStyle]}>
               {opts.bullet}
             </TextComponent>);
@@ -142,7 +163,7 @@ export default function htmlToElement(rawHtml, customOpts = {}, done) {
           }
         }
 
-        const {NodeComponent, styles} = opts;
+        const { NodeComponent, styles } = opts;
 
         return (
           <NodeComponent
@@ -163,7 +184,9 @@ export default function htmlToElement(rawHtml, customOpts = {}, done) {
   }
 
   const handler = new htmlparser.DomHandler(function(err, dom) {
-    if (err) done(err);
+    if (err) {
+      done(err);
+    }
     done(null, domToElement(dom));
   });
   const parser = new htmlparser.Parser(handler);
